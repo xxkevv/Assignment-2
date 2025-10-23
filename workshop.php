317da01bff006b83d36fb5d2e49ff31e1c7de940
Kevinn Jose Novellino Shen <shenkevinn8@gmail.com>
2025-10-23 14:33:42 +0800

    Added to footer.php to all pages

--- workshop.php
+++ workshop.php
@@ -157,50 +157,7 @@
     </main>
   
     <!-- Footer  -->
-    <footer class="footer-content">
-    <div class="footer-container">
-         <div class="other">
-        <h2>Others</h2>
-        <ul class="box">
-            <li> <a href="enhancement.html">Enhancement</a></li>
-            <li> <a href="acknowledge.html">Acknowledge</a></li>
-            <li> <a href="https://youtu.be/Wnv-ve2rwts?si=N3-SYA9JKMNv45jn">Group Presentation</a>
-        </ul>
-        </div>
-        <div class="quicklinks">
-            <h2>Products</h2>
-            <ul class="box">
-                <li><a href="product1.html">Hand-bouquet</a> </li>
-                <li><a href="product2.html">CNY decoration</a> </li>
-                <li><a href="product3.html">Grand Opening</a> </li>
-                <li><a href="product4.html">Graduation</a> </li>
-            </ul>
-        </div>
-
-        <div class="quicklinks">
-            <h2> Sign Up </h2>
-            <ul class="box">
-                <li><a href="register.html">Workshop</a> </li>
-                <li><a href="membership.html">Membership</a> </li>
-            </ul>
-        </div>
-
-        <div class="contact">
-            <h2>Contact Us</h2>
-            <ul class="box">
-                <li> <img src="IMAGE/location.svg" alt="Location"> <p> Lorong Bdc, 93350 Kuching, Sarawak </p> 
-                </li>
-
-                <li> <img src="IMAGE/envelope.svg" alt="Email"><p> Email: <a href="mailto:105801952@students.swinburne.edu.my">therootflower@gmail.com</a></p> 
-                </li>
-            </ul>
-        </div>
-        </div>
-        <div class="copyright">
-        <p>&copy; 2025 Root Flower. All rights reserved.</p>
-        </div>
-
-    </footer>
+    <?php include("footer.php"); ?>
 
     <a href="profile.html" class="profile-icon" title="Go to our profile page">
         <img src="IMAGE/profile.svg" alt="Profile">
